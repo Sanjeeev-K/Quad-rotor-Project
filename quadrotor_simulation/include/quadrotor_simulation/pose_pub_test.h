dc0504e172c388f5f422fe4783a913b8bf5c7fb8
Zhanghq8 <hzhang8@wpi.edu>
2019-12-12 23:22:48 -0500

    update new pose_pub

--- quadrotor_simulation/include/quadrotor_simulation/pose_pub_test.h
+++ quadrotor_simulation/include/quadrotor_simulation/pose_pub_test.h
@@ -11,6 +11,8 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseStamped.h"
+#include <visualization_msgs/Marker.h> 
+#include <visualization_msgs/MarkerArray.h>  
 
 // Input: vector<vector<double>> pose;
 // Ex/: vector<vector<double>> path{{0,0,0,0,0,0,1},{2,2,2,0,0,0,1},{4,4,4,0,0,0,1},{6,6,6,0,0,0,1},{8,8,8,0,0,0,1}};
@@ -22,12 +24,12 @@ using namespace std;
 class PosePubTest
 {
 private:
-	vector<vector<double>> path;
-	vector<double> currentPose;
-	vector<double> goalPose;
-	double reachedCheck = 0.2;
-	int path_index = 0;
-	bool reached = true;
+    vector<vector<double>> path;
+    vector<double> currentPose;
+    vector<double> goalPose;
+    double reachedCheck = 0.3;
+    int path_index = 0;
+    bool reached = true;
     ros::NodeHandle nh_;
     ros::Subscriber currentpos_sub_;
     ros::Subscriber stop_sub_;
@@ -36,6 +38,9 @@ private:
     geometry_msgs::Twist controlinput;
     // geometry_msgs::PoseStamped current_pose;
     geometry_msgs::PoseStamped goal_pose;
+    visualization_msgs::MarkerArray path_marker_array;
+    visualization_msgs::Marker path_marker;
+    ros::Publisher markerarray_pub_;
 
 public:
     PosePubTest(ros::NodeHandle* nodehandle, vector<vector<double>> path);
@@ -43,7 +48,6 @@ public:
     void initPub();  
     void initVec();
     void setgoalpos();  
-    // void currentposCallback(const geometry_msgs::PoseStamped& odom1);
     void eventCallback(const geometry_msgs::PoseStamped& odom1); 
     ~PosePubTest();
 };
